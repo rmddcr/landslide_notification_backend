d7d12ebb5a00b5d99952a6ef0678194e500891ef
Damitha Ranasinghe <rmddcr@gmail.com>
2018-08-19 22:03:38 +0530

    edited server code

--- server.js
+++ server.js
@@ -44,11 +44,46 @@ var users = require('./routes/users');
 
 
 
+
+// BodyParser Middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser());
+
 
 app.set("view engine", "hbs");
+app.engine('handlebars', exphbs({defaultLayout:'layout'}));//modified code 
 app.set("views", path.join(__dirname, "views"));
+
+
+
+// Express Session
+app.use(session({
+    secret: 'secret',
+    saveUninitialized: true,
+    resave: true
+}));
+
+
+
+// Express Validator
+app.use(expressValidator({
+    errorFormatter: function(param, msg, value) {
+        var namespace = param.split('.')
+        , root    = namespace.shift()
+        , formParam = root;
+  
+      while(namespace.length) {
+        formParam += '[' + namespace.shift() + ']';
+      }
+      return {
+        param : formParam,
+        msg   : msg,
+        value : value
+      };
+    }
+  }));
+  
 
 // app.get('/qtest/all', (req, res) => {
 //     db.fetchTasks(function (result) {
